ef4fb465c37aa4ba279ce9a7ebde11c7293525e0
adriantwardy <adriantwardawski@gmail.com>
2021-12-28 13:53:16 +0100

    KonfiguracjaLogera

--- RestaurantAPI2/Services/RestaurantService.cs
+++ RestaurantAPI2/Services/RestaurantService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RestaurantAPI2.Entities;
 using RestaurantAPI2.Models;
 using System;
@@ -15,11 +16,13 @@ namespace RestaurantAPI2.Services
     {
         private readonly RestaurantDbContext _dbContext;
         private readonly IMapper _mapper;
+        private readonly ILogger<RestaurantService> _logger;
 
-        public RestaurantService(RestaurantDbContext dbContext, IMapper mapper)
+        public RestaurantService(RestaurantDbContext dbContext, IMapper mapper, ILogger<RestaurantService> logger)
         {
             _dbContext = dbContext;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public RestaurantDto GetById(int id)
@@ -60,6 +63,8 @@ namespace RestaurantAPI2.Services
 
         public bool Delete(int id)
         {
+            _logger.LogError($"Restaurant with id: {id} DELETE action invoked");
+
             var restaurant = _dbContext
               .Restaurants             
               .FirstOrDefault(r => r.Id == id);
