4d11692819deb9ac7b4a92cb94c61e4f308b2e60
adriantwardy <adriantwardawski@gmail.com>
2021-12-28 14:50:38 +0100

    AddErrorHandlingMiddleware

--- RestaurantAPI2/Startup.cs
+++ RestaurantAPI2/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RestaurantAPI2.Entities;
+using RestaurantAPI2.Middleware;
 using RestaurantAPI2.Services;
 using System;
 using System.Collections.Generic;
@@ -32,6 +33,7 @@ namespace RestaurantAPI2
             services.AddScoped<RestaurantSeeder>(); //rejestracja serwisu seeduj¹cego
             services.AddAutoMapper(this.GetType().Assembly); //rejestracja AutoMappera
             services.AddScoped<IRestaurantService, RestaurantService>(); //rejestacja serwisu do obs³ugi metod kontrolera
+            services.AddScoped<ErrorHandlingMiddleware>(); //rejestracja serwisu do obs³ugi wyj¹tków
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +45,7 @@ namespace RestaurantAPI2
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseHttpsRedirection();
 
