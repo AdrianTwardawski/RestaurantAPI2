8455d9f860aca02d6bf75232ab85f3504e17474d
adriantwardy <adriantwardawski@gmail.com>
2022-01-06 18:36:40 +0100

    PrzesyłaniePlikówNaServer

--- RestaurantAPI2/Controllers/FileController.cs
+++ RestaurantAPI2/Controllers/FileController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using System;
@@ -26,6 +27,24 @@ namespace RestaurantAPI2.Controllers
             contentProvider.TryGetContentType(fileName, out string contentType);
             var fileContents = System.IO.File.ReadAllBytes(filePath);
             return File(fileContents, contentType, fileName);
+        }
+
+        [HttpPost]
+        public ActionResult Upload([FromForm]IFormFile file)
+        {
+            if(file != null && file.Length > 0 )
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fileName = file.FileName;
+                var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok();
+            }
+            return BadRequest();
         }
     }
 }
