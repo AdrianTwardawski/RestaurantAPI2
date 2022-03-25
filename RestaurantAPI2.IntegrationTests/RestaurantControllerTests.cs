9dcf494ec6196a51de27463195cfd9fda5c13245
adriantwardy <adriantwardawski@gmail.com>
2022-03-25 10:39:38 +0100

    shared test context

--- RestaurantAPI2.IntegrationTests/RestaurantControllerTests.cs
+++ RestaurantAPI2.IntegrationTests/RestaurantControllerTests.cs
@@ -5,25 +5,32 @@ using System.Threading.Tasks;
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
 using FluentAssertions;
+using System.Net.Http;
 
 namespace RestaurantAPI2.IntegrationTests
 {
-    public class RestaurantControllerTests
+    public class RestaurantControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
+
+        private HttpClient _client;
+
+        public RestaurantControllerTests(WebApplicationFactory<Startup> factory)
+        {
+           //var factory = new WebApplicationFactory<Startup>();  //Tworzymy fabrykę do której przekazujemy typ statupu. Klasa ta przyjmuje jako parametr generyczny entry point, czyli klasę startową projektu,
+                                                                 //Dzięki temu klasa WebApplicationFactory będzie wiedziała w jaki sposób uruchomić projekt i odpowiednio go skonfiguruje
+            _client = factory.CreateClient();  // za pomocą tego klienta możemy odwołać się do różnych metod z naszego API
+        }
+
+
         [Theory]
         [InlineData("pageSize=5&pageNumber=1")]
         [InlineData("pageSize=10&pageNumber=2")]
         [InlineData("pageSize=15&PageNumber=3")]
         public async Task GetAll_WithCorrectQueryParameters_ReturnsOkResult(string queryParamas)
-        {
-            // arrange
-
-            var factory = new WebApplicationFactory<Startup>();  //Tworzymy fabrykę do której przekazujemy typ statupu. Klasa ta przyjmuje jako parametr generyczny entry point, czyli klasę startową projektu,
-                                                                 //Dzięki temu klasa WebApplicationFactory będzie wiedziała w jaki sposób uruchomić projekt i odpowiednio go skonfiguruje
-            var client = factory.CreateClient();  // za pomocą tego klienta możemy odwołać się do różnych metod z naszego API
+        {    
             // act
 
-            var response = await client.GetAsync("/api/restaurant?" + queryParamas);
+            var response = await _client.GetAsync("/api/restaurant?" + queryParamas);
 
             // assert
 
@@ -40,14 +47,9 @@ namespace RestaurantAPI2.IntegrationTests
         [InlineData("")]
         public async Task GetAll_WithInvalidQueryParameters_ReturnsBadRequest(string queryParams)
         {
-            // arrange
-            var factory = new WebApplicationFactory<Startup>();
-
-            var client = factory.CreateClient();
-
             // act
 
-            var response = await client.GetAsync("/api/restaurant?" + queryParams);
+            var response = await _client.GetAsync("/api/restaurant?" + queryParams);
 
             // assert
 
