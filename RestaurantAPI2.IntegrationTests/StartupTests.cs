18087f81810f0dccfebf8734e38060f0e103663d
adriantwardy <adriantwardawski@gmail.com>
2022-03-30 12:06:37 +0200

    Dependency Injection Tests

---
+++ RestaurantAPI2.IntegrationTests/StartupTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using RestaurantAPI2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantAPI2.IntegrationTests
+{
+    public class StartupTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly List<Type> _controllerTypes;
+        private readonly WebApplicationFactory<Startup> _factory;
+        public StartupTests(WebApplicationFactory<Startup> factory)
+        {
+            _controllerTypes = typeof(Startup)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(ControllerBase)))
+                .ToList();
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    _controllerTypes.ForEach(c => services.AddScoped(c));
+                });
+            });
+        }
+
+        [Fact]
+        //w tym teście chcemy sprawdzić czy kontener dependency injection poprawnie zarejestrował wszystkie zależności
+        public void ConfigureServices_ForControllers_RegistersAllDependencies() 
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+
+
+            // assert
+            _controllerTypes.ForEach(t =>
+            {
+                var controller = scope.ServiceProvider.GetService(t);
+                controller.Should().NotBeNull();
+            });
+        }
+    }
+}
