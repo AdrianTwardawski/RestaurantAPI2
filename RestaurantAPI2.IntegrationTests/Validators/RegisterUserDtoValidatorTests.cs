388214b65a6db2195b494aae9a68b4ce2558be83
adriantwardy <adriantwardawski@gmail.com>
2022-03-30 19:59:02 +0200

    Test Validator with dependecies

---
+++ RestaurantAPI2.IntegrationTests/Validators/RegisterUserDtoValidatorTests.cs
@@ -0,0 +1,87 @@
+﻿using FluentValidation.TestHelper;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI2.Entities;
+using RestaurantAPI2.Models;
+using RestaurantAPI2.Models.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestaurantAPI2.IntegrationTests.Validators
+{
+    public class RegisterUserDtoValidatorTests
+    {
+        private RestaurantDbContext _dbContext;
+
+        public RegisterUserDtoValidatorTests()
+        {
+            var builder = new DbContextOptionsBuilder<RestaurantDbContext>();
+            builder.UseInMemoryDatabase("TestDb");
+
+            _dbContext = new RestaurantDbContext(builder.Options);
+            Seed();
+        }
+
+        public void Seed()
+        {
+            var testUsers = new List<User>()
+            {
+                new User()
+                {
+                    Email = "test2@test.com"
+                },
+                new User()
+                {
+                    Email = "test3@test.com"
+                }
+            };
+
+            _dbContext.Users.AddRange(testUsers);
+            _dbContext.SaveChanges();
+        }
+
+        [Fact]
+        public void Validate_ForValidModel_ReturnsSuccess()
+        {
+             // arrange
+             var model = new RegisterUserDto()
+             {
+                 Email = "test@test.com",
+                 Password = "pass123",
+                 ConfirmPassword = "pass123"
+              };
+        
+            var validator = new RegisterUserDtoValidator(_dbContext);
+
+            // act
+            var result = validator.TestValidate(model);
+
+            // assert
+            result.ShouldNotHaveAnyValidationErrors();
+
+        }
+
+        [Fact]
+        public void Validate_ForInValidModel_ReturnsFailure()
+        {
+            // arrange
+            var model = new RegisterUserDto()
+            {
+                Email = "test2@test.com",
+                Password = "pass123",
+                ConfirmPassword = "pass123"
+            };
+
+            var validator = new RegisterUserDtoValidator(_dbContext);
+
+            // act
+            var result = validator.TestValidate(model);
+
+            // assert
+            result.ShouldHaveAnyValidationError();
+
+        }
+    }
+}
